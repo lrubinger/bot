ced87dfbaf9a10a0da28dcffb8b1544149fb6874
Lucas Rubinger <lucas.rubinger@gmail.com>
2026-09-23 19:29:06 -0300

    Add internal chat message model

---
+++ backend/src/models/InternalChatMessage.ts
@@ -0,0 +1,45 @@
+import {
+  Table, Column, CreatedAt, UpdatedAt, Model, PrimaryKey, AutoIncrement,
+  ForeignKey, BelongsTo, DataType
+} from "sequelize-typescript";
+import User from "./User";
+import Company from "./Company";
+
+@Table
+class InternalChatMessage extends Model<InternalChatMessage> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @ForeignKey(() => User)
+  @Column
+  senderId: number;
+
+  @BelongsTo(() => User, "senderId")
+  sender: User;
+
+  @ForeignKey(() => User)
+  @Column
+  recipientId: number;
+
+  @BelongsTo(() => User, "recipientId")
+  recipient: User;
+
+  @ForeignKey(() => Company)
+  @Column
+  companyId: number;
+
+  @BelongsTo(() => Company)
+  company: Company;
+
+  @Column(DataType.TEXT)
+  body: string;
+
+  @CreatedAt
+  createdAt: Date;
+
+  @UpdatedAt
+  updatedAt: Date;
+}
+export default InternalChatMessage;
