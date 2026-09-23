51023063cdc7ce9e502a4aaa54b889801642b1f3
Lucas Rubinger <lucas.rubinger@gmail.com>
2026-09-23 19:09:45 -0300

    Expose chatbot settings through stable user routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,12 +2,19 @@ import { Router } from "express";
 
 import isAuth from "../middleware/isAuth";
 import * as UserController from "../controllers/UserController";
+import * as ProfileController from "../controllers/ProfileController";
 
 const userRoutes = Router();
 
 userRoutes.get("/users", isAuth, UserController.index);
 
 userRoutes.get("/users/list", isAuth, UserController.list);
+
+// PortoPlan Chatbot profile/settings routes. Kept under /users so they also
+// work in deployments that proxy the existing user API paths explicitly.
+userRoutes.get("/users/profile/list", isAuth, ProfileController.list);
+userRoutes.get("/users/profile/:userId", isAuth, ProfileController.show);
+userRoutes.put("/users/profile/:userId", isAuth, ProfileController.update);
 
 userRoutes.post("/users", isAuth, UserController.store);
 
