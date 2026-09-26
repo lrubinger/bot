f627ed577a5d8c5fd7b8ef43953b940681e11068
Lucas Rubinger <lucas.rubinger@gmail.com>
2026-09-26 08:05:36 -0300

    Adiciona rotas CSV de contatos

--- backend/src/routes/contactRoutes.ts
+++ backend/src/routes/contactRoutes.ts
@@ -1,15 +1,32 @@
 import express from "express";
+import multer from "multer";
 import isAuth from "../middleware/isAuth";
+import uploadConfig from "../config/upload";
 
 import * as ContactController from "../controllers/ContactController";
 import * as ImportPhoneContactsController from "../controllers/ImportPhoneContactsController";
+import * as ContactCsvController from "../controllers/ContactCsvController";
 
 const contactRoutes = express.Router();
+const upload = multer(uploadConfig);
 
 contactRoutes.post(
   "/contacts/import",
   isAuth,
   ImportPhoneContactsController.store
+);
+
+contactRoutes.post(
+  "/contacts/import-csv",
+  isAuth,
+  upload.single("file"),
+  ContactCsvController.importCsv
+);
+
+contactRoutes.get(
+  "/contacts/export-csv",
+  isAuth,
+  ContactCsvController.exportCsv
 );
 
 contactRoutes.get("/contacts", isAuth, ContactController.index);
